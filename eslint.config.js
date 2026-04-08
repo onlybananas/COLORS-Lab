5b60272973e8e00c24dce869f0656a68aeb5d95a
only.bananas <an431589@ucf.edu>
2026-04-07 23:47:56 -0400

    added GitHub Actions workflow and linting

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "script",
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
